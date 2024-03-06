ff9eed8175076d16fce032d9423209f2cc215252
junnyctang <junnoonlagoon@gmail.com>
2024-03-05 20:26:03 -0800

    finished first parts

---
+++ javascript.js
@@ -0,0 +1,32 @@
+/*function getComputerChoice(Rock, Paper, Scissors){
+    return Math.random();
+}
+
+console.log(getComputerChoice(choice)) 
+
+function getRandom(){
+    return Math.random();
+}
+
+console.log(getRandom()) */
+
+/*const choices = ["rock", "paper", "scissors"];
+console.log(choices[Math.floor(Math.random() * (0 - 3) * (-1))])*/
+
+
+function getComputerChoice(){
+    
+    const choices = ['Rock', 'Paper', 'Scissors']
+    return choices[Math.floor(Math.random() * (3 - 0) + 0)]
+}
+/*console.log(getComputerChoice()) */
+
+function playground(playerSelection, computerSelection){
+
+    if playerSelection
+    
+} 
+
+const playerSelection = 'paper';
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
